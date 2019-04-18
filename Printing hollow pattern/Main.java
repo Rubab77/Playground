29e0bbcbf1ffea79401f1818be5c7c3633f12da7
Rubab77 <47818633+Rubab77@users.noreply.github.com>
2019-04-18 08:49:00 +0530

    Test code pushed to repository.

---
+++ Printing hollow pattern/Main.java
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+	int n;
+  scanf("%d",&n);
+ for(int curr_row = 1 ; curr_row <= n ; curr_row++)
+ {
+   // handle stars for each row
+   for( int curr_col = 1 ; curr_col <= n ; curr_col++)
+   {
+     //condition to print stars
+     if((curr_row == 1) || (curr_col == 1)|| (curr_row == n)||(curr_col == n))
+     {
+       printf("*");
+     }
+     else{
+       printf(" ");
+     }
+   }
+   printf("\n");
+ }
+	return 0;
+}
